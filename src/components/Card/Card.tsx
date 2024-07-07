476d18a0a949f1d94b4c0d100c4fc580207df1e4
Shafnaa <saujanashafi2003@gmail.com>
2024-07-07 17:23:09 +0700

    remove unused

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,4 @@
 "use client";
-import EngineSwitcher from "../Switchers/EngineSwitcher";
-
 export default function Card({ children, title }: any) {
   return (
     <div className="col-span-12 flex flex-col items-center justify-center rounded-xl border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:col-span-4">
